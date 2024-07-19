e70ba462e97cb545d26264329cbe7c72c79fbd9d
itsrusty <rusty3031@gmail.com>
2024-07-18 19:22:25 -0700

    [feature]: matches

--- src/matchs/matchs.controller.ts
+++ src/matchs/matchs.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { MatchsService } from './matchs.service';
 
 @Controller('matchs')
 export class MatchsController {
-  constructor(private readonly matchsService: MatchsService) {}
+  constructor(private readonly matchsService: MatchsService) { }
+
+  @Get('/user/matchs/:userId')
+  public matchUsers(@Param('userId', ParseIntPipe) userId: number) {
+    return this.matchsService.findMatchsByUser(userId);
+  }
+
+  // @Get('/user/matchs/create/')
+  // public createMatchWeekly() {
+  //   return this.matchsService.createMatchWeekly();
+  // }
 }
--- src/matchs/matchs.service.ts
+++ src/matchs/matchs.service.ts
@@ -2,22 +2,62 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 // import { CONFLICT_ERROR } from "apicustomerrors"
 import cron from 'node-cron';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class MatchsService {
-    constructor(private prisma: PrismaService) {}
+    constructor(private prisma: PrismaService) { }
 
-    async findMatchsByUser() { }
+    public async getSkillsUser(userId: number): Promise<string[]> {
+        const user = await this.prisma.user.findUniqueOrThrow({
+            where: { id: userId },
+            select: { skills: true },
+        });
 
-    async createMatchDaily() {
-        try {
-            // const allUsers
-        } catch (error) {
-            console.error(error.message);
-            throw new HttpException(
-                'error in service',
-                HttpStatus.BAD_REQUEST
-            );
-        }
+        return user?.skills || [];
     }
+
+    public async findMatchsByUser(userId: number): Promise<User[] | Object> {
+        const userSkills = await this.getSkillsUser(userId);
+
+        if (!userSkills.length) return [];
+
+        const usersMatches = await this.prisma.user.findMany({
+            where: {
+                AND: [
+                    { id: { not: userId } },
+                    { skills: { hasSome: userSkills } }, // users with one skills common
+                ],
+            },
+            select: {
+                id: true,
+                username: true,
+                description: true,
+                descriptionLong: true,
+                avatar_url: true,
+                skills: true,
+            },
+        });
+
+        const scoreMatches = usersMatches.map((user) => {
+            const lengthCommonSkills = user.skills.filter((skill) =>
+                userSkills.includes(skill),
+            ).length;
+
+            return { user, lengthCommonSkills };
+        });
+
+        return scoreMatches.sort(
+            (a, b) => b.lengthCommonSkills - a.lengthCommonSkills,
+        );
+    }
+
+    // public async createMatchWeekly() {
+    //     try {
+    //         // const allUsers
+    //     } catch (error) {
+    //         console.error(error.message);
+    //         throw new HttpException('error in service', HttpStatus.BAD_REQUEST);
+    //     }
+    // }
 }
